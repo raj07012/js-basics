c1be5e0b2a1c870de7f796ef937c68cb62e77dc4
rb161161 <rb161161@gmail.com>
2025-07-16 07:28:48 +0000

    Added datatypes and objects

---
+++ datatypes.js
@@ -0,0 +1,21 @@
+let productPrice = 2500;
+
+let productName = "Apple Iphone 11 Pro max";
+
+let isAvailable = true;
+
+let minusZero = -0
+
+console.log(minusZero);
+
+let x;
+
+console.log(x);
+
+let y = null;
+
+console.log(y);
+
+let veryBigNumber = 11443164646666645585523669663252223663n;
+
+console.log(veryBigNumber);
---
+++ objects.js
@@ -0,0 +1,15 @@
+let product = {
+    name: "Apple",
+    price: 1255,
+    discount: 15, 
+    isAvailable: false
+}
+
+let socialHandles = {
+    fb: 1700,
+    insta: 788,
+    X: 1002,
+    linkedIn: 4588
+}
+
+console.log(socialHandles);
